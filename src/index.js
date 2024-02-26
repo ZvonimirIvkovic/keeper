8574b9e1b7e9f40da8855bb2413929b408a11aec
Zvonimir Ivković <145219655+ZvonimirIvkovic@users.noreply.github.com>
2024-02-26 15:49:25 +0100

    Finishing code for part 1 of Keeper App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hellow World!</h1>
+  <div>
+    <App/>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
