8bfffaf07688acbb6500b59494f2145317f89043
Zvonimir Ivković <145219655+ZvonimirIvkovic@users.noreply.github.com>
2024-03-12 14:34:24 +0100

    Finising front-end of keeper App

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { Delete } from "@material-ui/icons";
 
-function Note({title, content}) {
-    return (
-        <div className="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
-        </div>
-    )
-};
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}><Delete/></button>
+    </div>
+  );
+}
 
 export default Note;
